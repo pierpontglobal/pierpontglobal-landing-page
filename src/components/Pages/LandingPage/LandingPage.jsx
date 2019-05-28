1f26dfdf1143c5860c5d401bbdf2ca6d215490c0
Hector Andres Acosta Pozo <hector.acosta.pozo@outlook.com>
2019-05-28 14:45:31 -0400

    Subscription details

--- src/components/Pages/LandingPage/LandingPage.jsx
+++ src/components/Pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import Greetings from "./Sections/Greetings/Greetings";
-import WhatWeDo from "./Sections/WhatWeDo/WhatWeDo";
-import Devices from "./Devices/Devices";
-import MainSections from "./Sections/MainSections/MainSections";
-import Footer from "./Sections/Footer/Footer";
-import Brands from "./Sections/Brands/Brands";
+import React from 'react';
+import Greetings from './Sections/Greetings/Greetings';
+import WhatWeDo from './Sections/WhatWeDo/WhatWeDo';
+import Devices from './Devices/Devices';
+import MainSections from './Sections/MainSections/MainSections';
+import Footer from './Sections/Footer/Footer';
+import Brands from './Sections/Brands/Brands';
 import styled from 'styled-components';
+import SubscriptionDetails from './Sections/SubscriptionDetails/SubscriptionDetails';
 
 const BrandsWrapper = styled.div`
   width: 50%;
@@ -16,10 +17,10 @@ const BrandsWrapper = styled.div`
   margin-right: auto;
 `;
 
-const LandingPage = props => (
+const LandingPage = (props) => (
   <div
     className="LandingPageWrapper"
-    style={{ overflow: "hidden", position: "relative" }}
+    style={{overflow: 'hidden', position: 'relative'}}
   >
     <Greetings />
     <Devices />
@@ -27,9 +28,9 @@ const LandingPage = props => (
     <BrandsWrapper>
       <Brands />
     </BrandsWrapper>
+    <SubscriptionDetails />
     <Footer />
   </div>
 );
-
 
 export default LandingPage;
