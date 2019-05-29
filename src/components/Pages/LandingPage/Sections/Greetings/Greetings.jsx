8aa18723ea29cefd63d41d2eadb6703f39121df5
Daniel Pena Berroa <digrape07@gmail.com>
2019-05-29 10:00:50 -0400

    video promotion added

--- src/components/Pages/LandingPage/Sections/Greetings/Greetings.jsx
+++ src/components/Pages/LandingPage/Sections/Greetings/Greetings.jsx
@@ -9,91 +9,143 @@ import {
 } from '../../LandingPage.styles';
 import './Greetings.css';
 import {Button} from '@material-ui/core';
+import IframeModal from '../../../../IframeModal/IframeModal';
+import styled from 'styled-components';
 
-const Greetings = (props) => {
-  return (
-    <Section>
-      <ul className="circles">
-        <li />
-        <li />
-        <li />
-        <li />
-        <li />
-        <li />
-        <li />
-        <li />
-        <li />
-        <li />
-      </ul>
-      <Stripes>
-        <StripeNode
-          style={{
-            width: '25%',
-            height: '20vh',
-            opacity: '0.3',
-            background: '#45B7E5',
-          }}
-        />
-        <StripeNode
-          style={{
-            left: '25%',
-            width: '20%',
-            height: '20vh',
-            opacity: '0.1',
-            background: '#05D5FF',
-          }}
-        />
-        <StripeNode
-          style={{
-            left: '45%',
-            width: '15%',
-            height: '20vh',
-            opacity: '0.1',
-            background: '#575D90',
-          }}
-        />
-        <StripeNode
-          style={{
-            bottom: '0',
-            right: '0',
-            width: '15%',
-            height: '20vh',
-            opacity: '0.4',
-            background: '#C1F0DA',
-          }}
-        />
-        <StripeNode
-          style={{
-            bottom: '0',
-            left: '0',
-            width: '15%',
-            height: '20vh',
-            opacity: '0.4',
-            background: '#C1F0DA',
-          }}
-        />
-      </Stripes>
-      <Content>
-        <Text>
-          <Heading>La mejor manera de comprar carros para tu dealer</Heading>
-          <TextBody style={{marginTop: '30px'}}>
-            PierpontGlobal es una plataforma que permite a los dealers de
-            República Dominicana participar en las subastas organizadas por
-            Manheim.
-          </TextBody>
+class Greetings extends React.Component {
 
-          <div>
-            <AccentButton style={{margin: '20px 20px 10px 0'}}>
-              Create account
-            </AccentButton>
-            <LightButton style={{margin: '20px 20px 10px 0'}}>
-              Contact sales
-            </LightButton>
-          </div>
-        </Text>
-      </Content>
-    </Section>
-  );
+  state = {
+    showVideo: false,
+    videoHeight: '80%',
+    videoWidth: '80%',
+    showOnlyVideoInModal: false,
+  }
+
+  componentDidMount = () => {
+    this.resizeVideoModal();
+    window.addEventListener('resize', () => {
+      this.resizeVideoModal();
+    });
+  }
+
+  resizeVideoModal = () => {
+    const { showOnlyVideoInModal, videoHeight } = this.state;
+    if (window.innerWidth <= 768) {
+      if (window.innerWidth <= 480) {
+        if (videoHeight !== '160px') {
+          this.setState({
+            videoHeight: '160px',
+            videoWidth: '95%',
+            showOnlyVideoInModal: true,
+          });
+        }
+      }
+    } else {
+      if ( showOnlyVideoInModal ) {
+        this.setState({
+          videoHeight: '80%',
+          videoWidth: '80%',
+          showOnlyVideoInModal: false,
+        });
+      }
+    }
+  }
+
+  togglePromoVideo = (value) => {
+    this.setState({
+      showVideo: value,
+    });
+  }
+
+  render() {
+    const { showVideo, videoHeight, videoWidth, showOnlyVideoInModal } = this.state;
+    return (
+      <>
+        <IframeModal open={showVideo} onlyChildren={showOnlyVideoInModal} src={'https://www.youtube.com/embed/XEcELW3hkuQ'} width={videoWidth} height={videoHeight} handleClose={() => this.togglePromoVideo(false)} />
+        <Section>
+          <ul className="circles">
+            <li />
+            <li />
+            <li />
+            <li />
+            <li />
+            <li />
+            <li />
+            <li />
+            <li />
+            <li />
+          </ul>
+          <Stripes>
+            <StripeNode
+              style={{
+                width: '25%',
+                height: '20vh',
+                opacity: '0.3',
+                background: '#45B7E5',
+              }}
+            />
+            <StripeNode
+              style={{
+                left: '25%',
+                width: '20%',
+                height: '20vh',
+                opacity: '0.1',
+                background: '#05D5FF',
+              }}
+            />
+            <StripeNode
+              style={{
+                left: '45%',
+                width: '15%',
+                height: '20vh',
+                opacity: '0.1',
+                background: '#575D90',
+              }}
+            />
+            <StripeNode
+              style={{
+                bottom: '0',
+                right: '0',
+                width: '15%',
+                height: '20vh',
+                opacity: '0.4',
+                background: '#C1F0DA',
+              }}
+            />
+            <StripeNode
+              style={{
+                bottom: '0',
+                left: '0',
+                width: '15%',
+                height: '20vh',
+                opacity: '0.4',
+                background: '#C1F0DA',
+              }}
+            />
+          </Stripes>
+          <Content>
+            <Text>
+              <Heading>La mejor manera de comprar carros para tu dealer</Heading>
+              <TextBody style={{marginTop: '30px'}}>
+                PierpontGlobal es una plataforma que permite a los dealers de
+                República Dominicana participar en las subastas organizadas por
+                Manheim.
+              </TextBody>
+    
+              <div>
+                <AccentButton style={{margin: '20px 20px 10px 0'}}>
+                  Create account
+                </AccentButton>
+                <LightButton style={{margin: '20px 20px 10px 0'}} onClick={() => this.togglePromoVideo(true)}>
+                  View promotion
+                </LightButton>
+              </div>
+            </Text>
+          </Content>
+        </Section>
+      </>
+    );
+  }
 };
 
 export default Greetings;
---
+++ src/components/ppg-modal/PPGModal.jsx
@@ -0,0 +1,91 @@
+/* eslint-disable require-jsdoc */
+import React from 'react';
+import Modal from '@material-ui/core/Modal';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Close from '@material-ui/icons/Close';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+const styles = theme => ({
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+    outline: 'none',
+    minHeight: '180px',
+    minWidth: '220px'
+  }
+});
+
+class PPGModal extends React.Component {
+
+  componentDidCatch(error, info) {
+    // You can also log the error to an error reporting service
+    console.log(error, info);
+  }
+
+  render() {
+    const {setOpen, classes, width, height, setPadding, onlyChildren, onBackAction} = this.props;
+    let style = {
+      margin: 'auto',
+      width: width,
+      height: height,
+      overflowY: 'scroll',
+      padding: '32px',
+    };
+  
+    if (!setPadding) {
+      style.padding = '48px 0px 0px 0px';
+    }
+
+    if (onlyChildren) {
+      style.padding = '0px 0px 0px 0px';
+    }
+  
+    return (
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={setOpen}
+        onClose={this.props.handleClose}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <div
+          style={style}
+          className={classes.paper}
+        >
+          {
+            onlyChildren ? this.props.children : (
+              <>
+                <div style={{ position: 'absolute', top: '5px', left: '5px' }}>
+                  {
+                    (onBackAction) ? (
+                      <IconButton onClick={this.props.onBackAction}>
+                        <ArrowBackIcon />
+                      </IconButton>
+                      ) 
+                    : null
+                  }
+              ``</div>
+                <div style={{ position: 'absolute', top: '5px', right: '5px' }}>
+                  <IconButton onClick={this.props.handleClose}>
+                    <Close />
+                  </IconButton>
+                </div>
+                { this.props.children }
+              </>
+            )
+          }
+        </div>
+      </Modal>
+    );
+  }
+}
+
+export default withStyles(styles)(PPGModal);
