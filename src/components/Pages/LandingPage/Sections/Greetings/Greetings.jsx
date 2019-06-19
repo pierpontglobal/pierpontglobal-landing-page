93660e630294c01f816e6d4fd168edb82b54e19f
Hector Andres Acosta Pozo <hector.acosta.pozo@outlook.com>
2019-06-19 09:26:09 -0400

    Local changes

--- src/components/Pages/LandingPage/Sections/Greetings/Greetings.jsx
+++ src/components/Pages/LandingPage/Sections/Greetings/Greetings.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import {Stripes, StripeNode, Content, Text} from './Greetings.styles';
+import React from "react";
+import { Stripes, StripeNode, Content, Text } from "./Greetings.styles";
 import {
   Section,
   Heading,
   TextBody,
   AccentButton,
-  LightButton,
-} from '../../LandingPage.styles';
-import './Greetings.css';
-import {Button} from '@material-ui/core';
+  LightButton
+} from "../../LandingPage.styles";
+import "./Greetings.css";
+import { Button } from "@material-ui/core";
 
-const Greetings = (props) => {
+const Greetings = props => {
   return (
     <Section>
       <ul className="circles">
@@ -28,65 +28,65 @@ const Greetings = (props) => {
       <Stripes>
         <StripeNode
           style={{
-            width: '25%',
-            height: '20vh',
-            opacity: '0.3',
-            background: '#45B7E5',
+            width: "25%",
+            height: "20vh",
+            opacity: "0.3",
+            background: "#45B7E5"
           }}
         />
         <StripeNode
           style={{
-            left: '25%',
-            width: '20%',
-            height: '20vh',
-            opacity: '0.1',
-            background: '#05D5FF',
+            left: "25%",
+            width: "20%",
+            height: "20vh",
+            opacity: "0.1",
+            background: "#05D5FF"
           }}
         />
         <StripeNode
           style={{
-            left: '45%',
-            width: '15%',
-            height: '20vh',
-            opacity: '0.1',
-            background: '#575D90',
+            left: "45%",
+            width: "15%",
+            height: "20vh",
+            opacity: "0.1",
+            background: "#575D90"
           }}
         />
         <StripeNode
           style={{
-            bottom: '0',
-            right: '0',
-            width: '15%',
-            height: '20vh',
-            opacity: '0.4',
-            background: '#C1F0DA',
+            bottom: "0",
+            right: "0",
+            width: "15%",
+            height: "20vh",
+            opacity: "0.4",
+            background: "#C1F0DA"
           }}
         />
         <StripeNode
           style={{
-            bottom: '0',
-            left: '0',
-            width: '15%',
-            height: '20vh',
-            opacity: '0.4',
-            background: '#C1F0DA',
+            bottom: "0",
+            left: "0",
+            width: "15%",
+            height: "20vh",
+            opacity: "0.4",
+            background: "#C1F0DA"
           }}
         />
       </Stripes>
       <Content>
         <Text>
           <Heading>La mejor manera de comprar carros para tu dealer</Heading>
-          <TextBody style={{marginTop: '30px'}}>
+          <TextBody style={{ marginTop: "30px" }}>
             PierpontGlobal es una plataforma que permite a los dealers de
             República Dominicana participar en las subastas organizadas por
             Manheim.
           </TextBody>
 
           <div>
-            <AccentButton style={{margin: '20px 20px 10px 0'}}>
+            <AccentButton style={{ margin: "20px 20px 10px 0" }}>
               Create account
             </AccentButton>
-            <LightButton style={{margin: '20px 20px 10px 0'}}>
+            <LightButton style={{ margin: "20px 20px 10px 0" }}>
               Contact sales
             </LightButton>
           </div>
